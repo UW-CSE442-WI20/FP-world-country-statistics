d94f5c9217da2e852cffed1a27fbb6320f978e26
Andrew Siew <aks42@cs.washington.edu>
2020-02-29 14:12:42 -0800

    cleaned up code and added working tabs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import Map from "./map.js";
 
-var map = new Map();
-map.render("United States of America", "navy");
-map.render("China", "steelblue");
+function renderMap(){
+    var map = new Map();
+    map.render("United States of America", "navy");
+    map.render("China", "steelblue");
+}
+
+function setTabs(){
+    document.getElementById("chart-tab").addEventListener("click", function (){
+        document.getElementById("graphs").className += " active show";
+        document.getElementById("world").classList.remove("show");
+        document.getElementById("world").classList.remove("active");
+        document.getElementById("tables").classList.remove("show");
+        document.getElementById("tables").classList.remove("active");
+    });
+    
+    document.getElementById("world-tab").addEventListener("click", function (){
+        document.getElementById("world").className += " active show";
+        document.getElementById("graphs").classList.remove("show");
+        document.getElementById("tables").classList.remove("show");
+        document.getElementById("graphs").classList.remove("active");
+        document.getElementById("tables").classList.remove("active");
+    });
+    document.getElementById("table-tab").addEventListener("click", function (){
+        document.getElementById("tables").className += " active show";
+        document.getElementById("world").classList.remove("show");
+        document.getElementById("graphs").classList.remove("show");
+        document.getElementById("world").classList.remove("active");
+        document.getElementById("graphs").classList.remove("active");
+    });
+}
+
+
+
+function init() {
+    renderMap();
+    setTabs();
+}
+
+init();
