bb33314b0d7fbe108a13c2a672599deb830cfe23
Andrew Siew <aks42@cs.washington.edu>
2020-03-02 11:55:33 -0800

    style pie chart more

--- src/pie-chart.js
+++ src/pie-chart.js
@@ -1,81 +1,42 @@
 const d3 = require("d3");
 
 async function makeDonut(final_data) {
-    let data = final_data;
-    document.getElementById('pie').innerHTML = "";
-    let text = '';
-
-    let width = 300;
-    let height = 300;
-    let thickness = 30;
-    let duration = 200;
-
-    let radius = Math.min(width, height) / 2;
-
-    let svg = d3.select('#pie')
-        .append('svg')
-        .attr('class', 'pie')
-        .attr('width', width)
-        .attr('height', height);
-
-    let g = svg.append('g')
-        .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
-
-    let arc = d3.arc()
-        .innerRadius(radius - thickness)
-        .outerRadius(radius);
-
-    let pie = d3.pie()
-        .value(function (d) { return d.value; })
-        .sort(null);
-
-    let path = g.selectAll('path')
-        .data(pie(data))
-        .enter()
-        .append('g')
-        .on('mouseover', function (d) {
-            let g = d3.select(this)
-                .style('cursor', 'pointer')
-                .style('fill', 'black')
-                .append('g')
-                .attr('class', 'text-group');
-
-            g.append('text')
-                .attr('class', 'name-text')
-                .text(`${d.data.name}`)
-                .attr('text-anchor', 'middle')
-                .attr('dy', '-1.2em');
-
-            g.append('text')
-                .attr('class', 'value-text')
-                .text(`${d.data.value}`)
-                .attr('text-anchor', 'middle')
-                .attr('dy', '.6em');
-        })
-        .on('mouseout', function (d) {
-            d3.select(this)
-                .style('cursor', 'none')
-                .style('fill', `${d.data.color}`)
-                .select('.text-group').remove();
-        })
-        .append('path')
-        .attr('d', arc)
-        .attr('fill', function (d) { return d.data.color; })
-        .on('mouseover', function (d) {
-            d3.select(this)
-                .style('cursor', 'pointer')
-                .style('fill', 'black');
-        })
-        .on('mouseout', function (d) {
-            d3.select(this)
-                .style('cursor', 'none')
-                .style('fill', `${d.data.color}`);
-        });
-
-    g.append('text')
-        .attr('text-anchor', 'middle')
-        .attr('dy', '.20em')
-        .text(text);
+    var ctx = document.getElementById("pie");
+    let label_data = final_data.map(x => x.name);
+    let value_data = final_data.map(x => x.value);
+    let color_data = final_data.map(x => x.color);
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: label_data,
+    datasets: [{
+      data: value_data,
+      backgroundColor: color_data,
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: true,
+      position: "bottom"
+    },
+    cutoutPercentage: 80,
+  },
+});
+    
 }
+
+
 
 export default makeDonut;
