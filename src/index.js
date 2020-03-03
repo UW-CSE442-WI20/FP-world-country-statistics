dce52541a57bb5fb8f21006043572886bd927fd6
Rebecca Bol <rbol@cs.washington.edu>
2020-03-03 14:16:46 -0800

    Clicking on map zooms in and displays stats

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import Map from "./map.js";
+import map from "./map.js";
 import makeDonut from "./pie-chart.js";
-import makeLineChart from "./line-chart.js";
-import makeBarChart from "./bar-chart.js";
-import makeRadarChart from "./radar-chart.js";
 const d3 = require("d3");
 const regeneratorRuntime = require("regenerator-runtime");
 
@@ -12,9 +9,8 @@ var countryToData = {};
 
 var data = require("./data.csv");
 function renderMap(){
-    var map = new Map();
-    map.render("United States of America", "navy");
-    map.render("China", "steelblue");
+    map(countryToData);
+    console.log(indicators);
 }
 
 function setTabs(){
@@ -57,6 +53,12 @@ function setTabs(){
     });
 }
 
+function setSelectPickers(){
+    $('#country-picker').selectpicker();
+    $('#data-picker').selectpicker();
+    $('#year-picker').selectpicker();
+}
+
 async function parseData() {
     await d3.csv(data, async function(row) {
         
@@ -92,77 +94,21 @@ async function fillFilters(){
     for(let i = 1990; i <= 2015; i++){
         yearFilter.innerHTML += "<option value='" + i + "'>" + i + "</option>";
     }
+    
+
 }
 
 async function generateCharts(){
     let pieChartColors = ['#4e73df', '#f6c23e', '#36b9cc', '#1cc88a', '#6f42c1', '#5a5c69'];
-    let secondaryChartColors = ['rgba(78, 115, 223, 0.2)', 'rgba(246, 194, 62, 0.2)', 'rgba(54, 185, 204, 0.2)', 'rgba(28, 200, 138, 0.2)', 'rgba(111, 66, 193, 0.2)', 'rgba(90, 92, 105, 0.2)'];
     let countryFilter = $("#country-picker").val();
     let dataFilter = $("#data-picker").val();
     let yearFilter = $("#year-picker").val();
     let donutData = [];
-
-    let barData = {};
-    let barLabel = getRangeYears(parseInt(yearFilter));
-    barData.labels = barLabel;
-    let barDataSet = [];
-    let lineDataSet = [];
-    let  lineData = {};
-    lineData.labels = barLabel;
-    let radarDataSet = [];
-    let  radarData = {};
-    radarData.labels = countryFilter;
+    
     for(let i = 0; i < countryFilter.length; i++){
         donutData.push({color: pieChartColors[i], name: countryFilter[i], value: countryToData[countryFilter[i]][dataFilter][yearFilter]});
-        let barTemp = getCountryData(countryFilter[i], dataFilter, barLabel);
-        
-        barDataSet.push({label: countryFilter[i], backgroundColor: pieChartColors[i], data: barTemp});
-
-        let lineTemp = getCountryData(countryFilter[i], dataFilter, barLabel);
-        lineDataSet.push({label: countryFilter[i], backgroundColor:secondaryChartColors[i] ,borderColor: pieChartColors[i], data: lineTemp, lineTension: 0.3, pointRadius: 3,
-            pointBorderWidth: 2});
-
-        
     }
-
-    for(let i = 0; i < barLabel.length; i++){
-        let year = barLabel[i];
-        let tempRadar = {label: year, backgroundColor:secondaryChartColors[i] ,
-            borderColor: pieChartColors[i],
-            fill: true};
-        let data =[];
-        for(let j = 0; j < lineDataSet.length; j++){
-            data.push(lineDataSet[j].data[i]);
-        }
-        tempRadar.data = data;
-        radarDataSet.push(tempRadar);
-    }
-    barData.datasets = barDataSet;
-    lineData.datasets = lineDataSet;
-    radarData.datasets = radarDataSet;
     makeDonut(donutData);
-    makeBarChart(barData);
-    makeLineChart(lineData);
-    makeRadarChart(radarData);
-
-}
-
-function getRangeYears(year){
-    if(year <= 1995){
-        return [1990, 1991, 1992, 1993, 1994, 1995];
-    }else if(year >= 2010){
-        return [2010, 2011, 2012, 2013, 2014, 2015];
-    }else{
-        return [year, year + 1, year + 2, year + 3, year  + 4, year + 5];
-    }
-}
-
-function getCountryData(country, dataType, years){
-    let ret = [];
-    for(let i = 0 ; i < years.length; i++){
-        ret.push(countryToData[country][dataType][years[i]]);
-    }
-    return ret;
 }
 
 async function enableGenerateButton(){
@@ -179,14 +125,9 @@ async function enableGenerateButton(){
         document.getElementById("generate_button").className += " active";
         document.getElementById("generate_button").disabled = false;
     }
-}
 
-async function initCharts(){
-    $("#country-picker").val(["Argentina", "Chile", "Hong Kong SAR, China"]);
-    $("#data-picker").val("Population, total");
-    $("#year-picker").val("2000");
-    $('.selectpicker').selectpicker('refresh');
-    generateCharts();
+
+    
 }
 
 async function init() {
@@ -198,7 +139,6 @@ async function init() {
     setTabs();
     await parseData();
     await fillFilters();
-    await initCharts();
 }
 
 init();
--- src/map.js
+++ src/map.js
@@ -1,37 +1,94 @@
 const topo = require("./geoworld.json");
 const d3 = require("d3");
+const cc = require("./countrycodes.js");
+var active = d3.select(null);
+var width, height, zoom, path, svg, g, data;
 
-export default class Map {
-  constructor() {
-    let map = d3.select("#map");
-    let width = map.node().getBoundingClientRect().width;
-    let height = width / 2;
+function map(d){
+  data = d;
+  let dim = d3.select("#map").node().getBoundingClientRect();
+  width = dim.width;
+  height = dim.width/2;
+  
+  zoom = d3.zoom().scaleExtent([1,6]).on("zoom", zoomed);
 
-    this.svg = map
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g");
+  path = d3.geoPath().projection(
+    d3
+      .geoNaturalEarth1()
+      .scale(width / 6)
+      .translate([width / 2, height / 2])
+  );
 
-    let path = d3.geoPath().projection(
-      d3
-        .geoEqualEarth()
-        .scale(width / 7)
-        .translate([width / 2, height / 2])
-    );
+  svg = d3.select("#map")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", stopped)
 
-    this.svg
-      .selectAll("path")
-      .data(topojson.feature(topo, topo.objects.countries).features)
-      .enter()
-      .append("path")
-      .attr("d", path);
-  }
+  svg.append("rect").attr("class", "background").attr("width", width).attr("height", height).on("click",reset);
+  g = svg.append("g");
 
-  render(data, color) {
-    this.svg
-      .selectAll("path")
-      .filter(d => d.properties.name === data)
-      .style("fill", color);
-  }
+  g
+    .selectAll("path")
+    .data(topojson.feature(topo, topo.objects.countries).features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "feature")
+    .on("click", focus);
+
+  svg.call(zoom);
 }
+
+function focus(node){
+  console.log(node);
+  if (active.node() === this) return reset();
+  active.classed("active", false);
+  active = d3.select(this).classed("active", true);
+
+  var bounds = path.bounds(node),
+      dx = bounds[1][0] - bounds[0][0],
+      dy = bounds[1][1] - bounds[0][1],
+      x = (bounds[0][0] + bounds[1][0]) / 2,
+      y = (bounds[0][1] + bounds[1][1]) / 2,
+      scale = .9 / Math.max(dx / width, dy / height),
+      translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+  svg.transition()
+      .duration(750)
+      .style("stroke-width", 1.5 / scale + "px")
+      .call(zoom.transform, d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale));
+  
+  displayData(node.properties.name);
+}
+  
+function reset() {
+  active.classed("active", false);
+  active = d3.select(null);
+  
+  svg.transition()
+      .duration(750)
+      .call(zoom.transform, d3.zoomIdentity);
+}
+function zoomed() {
+  g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+  g.attr("transform", d3.event.transform);
+}
+
+function displayData(country) {
+  let stats = data[country];
+  console.log(cc.cc);
+  let html = "<div class='map-data-header'><div><h1>" + country + "</h1></div><img src='https://www.countryflags.io/" + cc.codes[country] + "/flat/64.png'></div>";
+  console.log(stats);
+  Object.keys(stats).forEach(key => {
+    html += key + " : " + stats[key][2015] + "<br/>";
+  });
+  d3.select("#map-data").html(html);
+  }
+
+function stopped() {
+  if (d3.event.defaultPrevented) d3.event.stopPropagation();
+}
+
+
+export default map;
